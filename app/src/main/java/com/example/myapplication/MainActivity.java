c9d0439725273385b5038334c6c4be071f9d3ff2
saad <saadh9290@gmail.com>
2021-11-08 18:02:45 +0500

    different activites

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,11 +10,15 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 Button rep;
+Button prac;
+Button exame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rep=findViewById(R.id.button);
+        prac=findViewById(R.id.practice);
+        exame=findViewById(R.id.exam);
         rep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -23,6 +27,24 @@ Button rep;
                 Intent intent = new
                         Intent(Intent.ACTION_VIEW, webpage);
                 startActivity(intent);
+            }
+        });
+        prac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+        exame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(MainActivity.this, MainActivity3.class);
+                startActivity(intent);
+
             }
         });
 
