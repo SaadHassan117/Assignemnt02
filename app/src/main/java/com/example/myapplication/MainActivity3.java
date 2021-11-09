fda05e93ce5930d1888e7c55dfa1edf646b09a5b
saad <saadh9290@gmail.com>
2021-11-09 19:31:34 +0500

    quiz practise

--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -2,13 +2,35 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class MainActivity3 extends AppCompatActivity {
-
+    RadioButton one,two,three,four;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        one=findViewById(R.id.radioButton5);
+        two=findViewById(R.id.radioButton6);
+        three=findViewById(R.id.radioButton7);
+        four=findViewById(R.id.radioButton8);
+        one.setText(" End of Throat");
+        two.setText(" Middle of Throat");
+        three.setText(" Start  of Throat");
+        four.setText(" None of Above");
+    }
+    public void func(View v)
+    {
+        RadioButton btn=findViewById(v.getId());
+        String text=btn.getText().toString();
+        if (text.equals( "End of Throat"))
+        {
+         btn.setBackgroundColor(Color.GREEN);
+        }
     }
 }
