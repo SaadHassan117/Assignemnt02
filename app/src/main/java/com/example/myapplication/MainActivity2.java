459f0ecb3b15215c651c776509d3edc7aeb591bf
SaadHassan117 <saadh9290@gmail.com>
2021-12-06 08:43:33 -0800

    up button

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,17 +1,29 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    Toolbar myChildToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        // my_child_toolbar is defined in the layout file
+         myChildToolbar =
+                (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
     public void function(View v)
     {
