1b1b3da43c99aecb77a8ef95bd7d2d9b9cd47f79
SaadHassan117 <saadh9290@gmail.com>
2021-12-06 08:17:10 -0800

    onclick

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,8 +7,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 Button rep;
@@ -58,9 +60,25 @@ Toolbar obj;
 
     }
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.hello:
+                Toast.makeText(getApplicationContext(),"hello", Toast.LENGTH_LONG).show();
+            case R.id.settinge:
+                Toast.makeText(getApplicationContext(),"Fav", Toast.LENGTH_LONG).show();
+
+
+            default:
+// If we got here, the user's action was not recognized.
+// Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 // Inflate the menu; this adds items to the action bar if it is
                 getMenuInflater().inflate(R.menu.menu,menu);
         return true;
     }
-}
+    }
